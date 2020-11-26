5b9bdb0bda9e1847df3a7eb8db6b2c321c958dd3
Damian Wojakowski <wojakowski.damian@gmail.com>
2020-11-26 16:03:14 +0100

    adding serach bar

--- 2-FRONTEND/movies-list/src/features/movies-list/SearchBar/SearchBar.js
+++ 2-FRONTEND/movies-list/src/features/movies-list/SearchBar/SearchBar.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 export default class SearchBar extends Component {
 
     render() {
-        return <input />
+        return <div>
+            <h5>Movies List</h5>
+            Filter by (select?) <input />
+        </div>
     }
 }
