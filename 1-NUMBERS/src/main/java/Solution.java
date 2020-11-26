c1c0b42db633d0b32853aa96f09c9e362a0b65a0
Damian Wojakowski <wojakowski.damian@gmail.com>
2020-11-26 13:16:43 +0100

    in case of 1 or less return 1

--- 1-NUMBERS/src/main/java/Solution.java
+++ 1-NUMBERS/src/main/java/Solution.java
@@ -1,6 +1,10 @@
 public class Solution {
 
     public int getValue(int value) {
+        if (value <= 1) {
+            return value;
+        }
+
         return 8;
     }
 
--- 1-NUMBERS/src/test/java/SolutionTest.java
+++ 1-NUMBERS/src/test/java/SolutionTest.java
@@ -5,9 +5,19 @@ import static org.junit.Assert.*;
 public class SolutionTest {
 
     @Test
-    public void GivenNumberSix_ShouldReturnEigt() {
+    public void GivenInputIs6_ShouldReturn8() {
         int expectedValue = 8;
         int givenInput = 6;
+
+        Solution solution = new Solution();
+
+        assertEquals(expectedValue, solution.getValue(givenInput));
+    }
+
+    @Test
+    public void GivenInputIsOneOrLess_ShouldReturn1() {
+        int expectedValue = 1;
+        int givenInput = 1;
 
         Solution solution = new Solution();
 
