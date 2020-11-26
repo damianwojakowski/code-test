8f310e46fb1f79aead7cf270e85cc47516c80468
Damian Wojakowski <wojakowski.damian@gmail.com>
2020-11-26 13:20:48 +0100

    code refactdoring, moving solution to a field

--- 1-NUMBERS/src/test/java/SolutionTest.java
+++ 1-NUMBERS/src/test/java/SolutionTest.java
@@ -1,26 +1,38 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
 public class SolutionTest {
 
+    Solution solution;
+
+    @BeforeEach
+    public void init() {
+        solution = new Solution();
+    }
+
     @Test
-    public void GivenInputIs6_ShouldReturn8() {
+    public void GivenInputIsSix_ShouldReturnEight() {
         int expectedValue = 8;
         int givenInput = 6;
 
-        Solution solution = new Solution();
-
         assertEquals(expectedValue, solution.getValue(givenInput));
     }
 
     @Test
-    public void GivenInputIsOneOrLess_ShouldReturn1() {
+    public void GivenInputIsOneOrLess_ShouldReturnOne() {
         int expectedValue = 1;
         int givenInput = 1;
 
-        Solution solution = new Solution();
-
         assertEquals(expectedValue, solution.getValue(givenInput));
     }
+
+//    @Test
+//    public void GivenInputIsEight_ShouldReturnTwentyOne() {
+//        int expectedValue = 21;
+//        int givenInput = 8;
+//
+//        assertEquals(expectedValue, solution.getValue(givenInput));
+//    }
 }
