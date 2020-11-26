3c5e12d60d9149ca1cddd56fc683df1b04560bad
Damian Wojakowski <wojakowski.damian@gmail.com>
2020-11-26 13:08:24 +0100

    write first test

--- 1-NUMBERS/src/main/java/Solution.java
+++ 1-NUMBERS/src/main/java/Solution.java
@@ -1,2 +1,7 @@
 public class Solution {
+
+    public int getValue(int value) {
+        return 8;
+    }
+
 }
--- 1-NUMBERS/src/test/java/SolutionTest.java
+++ 1-NUMBERS/src/test/java/SolutionTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.*;
 
 public class SolutionTest {
 
+    @Test
+    public void GivenNumberSix_ShouldReturnEigt() {
+        int expectedValue = 8;
+        int givenInput = 6;
+
+        Solution solution = new Solution();
+
+        assertEquals(expectedValue, solution.getValue(givenInput));
+    }
 }
